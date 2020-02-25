b061dd4b7d6c0ca73a994a36858e9c3be25ff4c5
vALT0r <valtord@gmail.com>
2020-02-25 04:03:12 -0300

    test david

--- app/src/main/java/com/smokingbrain/cannedsmoke/MainActivity.kt
+++ app/src/main/java/com/smokingbrain/cannedsmoke/MainActivity.kt
@@ -3,7 +3,7 @@ package com.smokingbrain.cannedsmoke
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() { /* asi */
+class MainActivity : AppCompatActivity() { /* asi david */
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
